29e3b19a28e106c2559d2ff04ab631be6eb39f23
kkyuang <god@god.com>
2023-10-12 16:32:33 +0900

    .

---
+++ NN.py
@@ -0,0 +1,16 @@
+#인공신경망을 정의하는 파이썬 코드
+
+class NN:
+    def __init__(self):
+        #변수 선언하기
+
+        #노드
+        
+        #가중치
+
+        #편향
+
+    def randomGene(self): #랜덤으로 인공신경망 초기화
+
+    def transToGene(self): #인공신경망 정보를 유전자로 변환
+        
